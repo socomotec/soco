c2d255598cb8048ead7377d7ba1974f104b89529
Reignok <eduardsimp_@hotmail.com>
2016-05-25 23:56:22 -0400

    edicion body
    ok.

--- Index.php
+++ Index.php
@@ -1,7 +1,14 @@
 <HTML>
 	<HEAD>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</HEAD>
 	<BODY>
+		<div class="container">
+			<a class="waves-effect waves-light btn">Click aca</a>
+			
+
+		</div>
 	</BODY>
 	<FOOTER>
 	</FOOTER>
