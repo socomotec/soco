cc071998082a0c55103b1c1da57d30dd24ea0c7e
socomtec <socomtec@gmail.com>
2016-05-26 01:52:34 -0300

    INICIO MENU ADMINISTRATIVO
    DEFINCION DE COLORES Y PORTES

---
+++ ADMINISTRADOR/menu_administrativo.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<title>SOCOMTEC</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link type="text/css" rel="stylesheet" href="../MATERIALIZE/css/materialize.min.css"  media="screen,projection"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">		
+	</HEAD>
+
+	<BODY>
+	
+		<nav>
+    		<div class="nav-wrapper blue lighten-2">
+      			<a href="#!" class="brand-logo">Logo</a>
+     			<ul class="right hide-on-med-and-down">
+        			<li><a href="sass.html"><i class="material-icons">search</i></a></li>
+        			<li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+        			<li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+        			<li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+      			</ul>
+    		</div>
+  		</nav>
+
+		<div class="row">
+
+      		<div class="col s2 m3 l3 teal lighten-4">
+
+      			<ul class="right hide-on-med-and-down">
+        			<li><a href="sass.html"><i class="material-icons">search</i></a></li>
+        			<li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+        			<li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+        			<li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+      			</ul>
+        
+      		</div>
+
+      		<div class="col s10 m9 l9">
+
+      			<!-- AQUÍ VA EL CONTENIDO QUE DESEO PONER -->
+
+        
+      		</div>
+
+    	</div>
+
+		<script type="text/javascript" src="../JS/jquery-2.2.4.min.js"></script>
+     	<script type="text/javascript" src="../MATERIALIZE/js/materialize.min.js"></script>
+
+	</BODY>
+</HTML>
