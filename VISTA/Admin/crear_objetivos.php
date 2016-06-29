43aea5c1738871da70751b1ecbb86569cea3e42a
socomtec <socomtec@gmail.com>
2016-06-29 15:14:34 -0300

    arreglo 2

--- VISTA/Admin/crear_objetivos.php
+++ VISTA/Admin/crear_objetivos.php
@@ -9,13 +9,19 @@
 		<title>Socomtec</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<script type="text/javascript" src="../../JS/jquery-2.2.4.min.js"></script>
+		
 		<link rel="stylesheet" href="../../FONT-AWESOME/css/font-awesome.min.css" />
 		<link type="text/css" rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />	
+		
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 		<script src="../bootstrap/js/bootstrap.js"></script>
-
 		<script type="text/javascript">
 
+			$(function() {
+    			$( "#fecha_inicio_obge" ).datepicker();
+ 			});
             
         </script>
 
@@ -34,33 +40,17 @@
 
 					<div class="form-group">
     					<label>Nombre objetivo general</label>
-    					<input type="email" class="form-control" id="nombre_obge" placeholder="Objetivo General">
+    					<input  class="form-control" id="nombre_obge" placeholder="Objetivo General">
   					</div>
 
   					<div class="form-group">
-    					<label>Nombre objetivo general</label>
-    					<input type="email" class="form-control" id="nombre_obge" placeholder="Objetivo General">
+    					<label>Fecha Inicio</label>
+    					<input class="form-control" id="fecha_inicio_obge" placeholder="Fecha inicio" type="text" />
   					</div>
 
   					
-    
-            	<div class="well">
-  <div id="datetimepicker2" class="input-append">
-    <input data-format="MM/dd/yyyy HH:mm:ss PP" type="text"></input>
-    <span class="add-on">
-      <i data-time-icon="icon-time" data-date-icon="icon-calendar">
-      </i>
-    </span>
-  </div>
-</div>
-<script type="text/javascript">
-  $(function() {
-    $('#datetimepicker2').datetimepicker({
-      language: 'en',
-      pick12HourFormat: true
-    });
-  });
-</script>
+
+
         
     
 					
